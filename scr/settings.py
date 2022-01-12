772ac720a96ff6a07d11a14711a8ef289492482a
Colin Wisdom <cdubwisdom@gmail.com>
2022-01-12 16:20:56 -0600

    [WIP] Screenshots in HTML Report

--- scr/settings.py
+++ scr/settings.py
@@ -1,5 +1,10 @@
+from pathlib import Path
 from os import path
 
 #paths for testing
 URL = 'https://www.google.com/'
-TEST_PATH = path.dirname(__file__)
+TEST_PATH = Path(__file__).parent
+PROJ_PATH = Path(__file__).parent.parent
+REPORT_PATH = path.join(PROJ_PATH, r"build\reports")
+
+print(TEST_PATH)
--- scr/tests/conftest.py
+++ scr/tests/conftest.py
@@ -36,3 +36,19 @@ def chrome_driver_init(request, path_to_chrome):
     driver.maximize_window()
     yield
     driver.quit()
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    pytest_html = item.config.pluginmanager.getplugin("html")
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, "extra", [])
+    if report.when == "call":
+        # always add url to report
+        xfail = hasattr(report, "wasxfail")
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            # only add additional html on failure
+            extra.append(pytest_html.extras.image(REPORT_PATH+"image.png"))
+            extra.append(pytest_html.extras.html("<div>Additional HTML</div>"))
+        report.extra = extra
