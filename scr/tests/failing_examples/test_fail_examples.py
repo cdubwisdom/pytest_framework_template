73c484d76d0809fd894b21bae8f604c67e9a00ee
Colin Wisdom <cdubwisdom@gmail.com>
2022-05-02 09:31:20 -0500

    BDD Improvements

--- scr/tests/failing_examples/test_fail_examples.py
+++ scr/tests/failing_examples/test_fail_examples.py
@@ -1,29 +1,40 @@
 import pytest
 from scr.tests.writer_helper import Writer  # use only for developing new test methods
 
+'''
+Feature: Search the internet
+    In order to find information
+    As a user
+    I want to see ONLY relevant content
+'''
+
 
 @pytest.mark.usefixtures('chrome_driver_init')
 class TestFailExamples:
+    # Scenario: Should not see irrelevant info from web search
     def test_should_not_display_irrelevant_results_from_web_search(self):
         # Given: I am on the Google Search Page
         # Defined in conftest.py
 
-        # When: I enter a query in search field
-        # And: I click 'Search' button
+        # When: I give a query to the search field
+        # And: I initiate the search
         self.home_page.web_search(search_query='cookies')
 
-        # Then: I can see a list of relevant web pages
-        assert self.web_results_page.is_expected_result_url_listed(search_result='https://docs.pytest.org/') is True # Test fails because this should be False
+        # Then: I can see a list of ONLY relevant web pages
+        assert self.web_results_page.is_expected_result_url_listed(
+            search_result='https://docs.pytest.org/') is True  # Test fails because this should be False
 
     @pytest.mark.xfail
+    # Scenario: Should not see irrelevant images from image search
     def test_should_not_display_irrelevant_results_from_img_search(self):
         # Given: I am on the Google Search Page
         # Defined in conftest.py
 
         # When: I click on 'Images' Tab
-        # And: I enter a query in search field
-        # And: I hit 'Enter' on keyboard (Image Search Page has no button)
+        # And: I give a query to the search field
+        # And: I initiate the search
         self.home_page.img_search(search_query='cookies')
 
-        # Then: I can see relevant images
-        assert self.img_results_page.is_expected_result_img_listed(search_result='pytest · PyPI') is True # Test fails because this should be False
+        # Then: I can see a list of ONLY relevant web pages
+        assert self.img_results_page.is_expected_result_img_listed(
+            search_result='pytest · PyPI') is True  # Test fails because this should be False
--- scr/tests/passing_examples/test_pass_examples.py
+++ scr/tests/passing_examples/test_pass_examples.py
@@ -1,27 +1,35 @@
 import pytest
 from scr.tests.writer_helper import Writer  # use only for developing new test methods
+'''
+Feature: Search the internet
+    In order to find information
+    As a user
+    I want to see relevant content
+'''
 
 
 @pytest.mark.usefixtures('chrome_driver_init')
 class TestPassExamples:
+    # Scenario: Should be able to see relevant info from web search
     def test_should_display_relevant_result_from_web_search(self):
         # Given: I am on the Google Search Page
         # Defined in conftest.py
 
-        # When: I enter a query in search field
-        # And: I click 'Search' button
+        # When: I give a query to the search field
+        # And: I initiate the search
         self.home_page.web_search(search_query='pytest')
 
         # Then: I can see a list of relevant web pages
         assert self.web_results_page.is_expected_result_url_listed(search_result='https://pytest.org/') is True
 
+    # Scenario: Should be able to see relevant image from image search
     def test_should_display_relevant_result_from_img_search(self):
         # Given: I am on the Google Search Page
         # Defined in conftest.py
 
-        # When: I click on 'Images' Tab
-        # And: I enter a query in search field
-        # And: I hit 'Enter' on keyboard (Image Search Page has no button)
+        # When: I navigate to the 'Images' Tab
+        # And: I give a query to the search field
+        # And: I initiate the search
         self.home_page.img_search(search_query='pytest')
 
         # Then: I can see relevant images
